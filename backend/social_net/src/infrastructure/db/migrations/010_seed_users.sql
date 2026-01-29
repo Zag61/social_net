546868bf780061f5e48f3ac7bf0750e23f068a85
Zag61 <pmd.fizicon@gmail.com>
2026-01-29 13:21:47 +0300

    more seed files

--- backend/social_net/src/infrastructure/db/migrations/010_seed_users.sql
+++ backend/social_net/src/infrastructure/db/migrations/010_seed_users.sql
@@ -11,8 +11,16 @@ VALUES ('f1111111-1111-1111-1111-111111111111','99f2a4dc-3657-4891-839f-29b13974
 ('f1111111-1111-1111-1111-111111111112','99f2a4dc-3657-4891-839f-29b1397452a6','cat.png','image/png','object_storage','social-net-files','seed-files/cat.png'),
 ('f1111111-1111-1111-1111-111111111113','99f2a4dc-3657-4891-839f-29b1397452a7','images','image/png','object_storage','social-net-files','seed-files/images.webp'),
 ('f1111111-1111-1111-1111-111111111114','99f2a4dc-3657-4891-839f-29b1397452a6','wall','image/png','object_storage','social-net-files','seed-files/wall.jpg');
+INSERT INTO files(owner_id, name, mime_type, storage_bucket, storage_key) 
+VALUES('99f2a4dc-3657-4891-839f-29b1397452a5', 'немцы.mp4', 'video/mp4', 'social-net-files', 'seed-files/немцы.mp4');
+INSERT INTO files(owner_id, name, mime_type, storage_bucket, storage_key) 
+VALUES('99f2a4dc-3657-4891-839f-29b1397452a5', 'aud_eng.wav', 'audio/wav', 'social-net-files', 'seed-files/aud_eng.wav');
 INSERT INTO post_files (post_id, file_id, ord)
 VALUES('d0ee5498-6b43-4a11-bdd5-bba1e97e7742','f1111111-1111-1111-1111-111111111114',0);
+INSERT INTO post_files (post_id, file_id, ord)
+VALUES('67e8e09a-dfe1-4afa-862c-99b9a494477e','b76bde49-4fe2-4326-b6bc-75dee216493a',0);
+INSERT INTO post_files (post_id, file_id, ord)
+VALUES('67e8e09a-dfe1-4afa-862c-99b9a494477e','c870cb64-76dd-4d3b-a84d-7e3b3f96a6b2',0);
 INSERT INTO friendships(requester_id, addressee_id, status)  VALUES('99f2a4dc-3657-4891-839f-29b1397452a5','99f2a4dc-3657-4891-839f-29b1397452a6', 'accepted');
 INSERT INTO messages(sender_id, receiver_user_id, text_f) VALUES('99f2a4dc-3657-4891-839f-29b1397452a5','99f2a4dc-3657-4891-839f-29b1397452a6', 'message from micke to vann');
 INSERT INTO messages(sender_id, receiver_user_id, text_f) VALUES('99f2a4dc-3657-4891-839f-29b1397452a6','99f2a4dc-3657-4891-839f-29b1397452a5', 'message from vann to micke');
